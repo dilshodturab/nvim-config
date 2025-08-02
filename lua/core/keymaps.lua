352cf84d5ba922767055e196ae562b2008ea22a4
dilshodturab <avlomberdioglid@gmail.com>
2025-08-02 19:53:31 +0500

    CTRL + s for saving file keymap added
    /

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,32 +2,33 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Disable the spacebar key's default behaviour in Normal and Visual modes
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', {silent = true })
+-- Disable the spacebar key's default behavior in Normal and Visual modes
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- For conciseness
 local opts = { noremap = true, silent = true }
 
 -- save file
-vim.keymap.set('n', 'C-s', '<cmd> w <CR>', opts)
+vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
 
--- save file without autoformatting
-vim.keymap.set('n', '<leader>sn', '<cmd> w <CR>', opts)
+-- save file without auto-formatting
+vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
 
 -- quit file
-vim.keymap.set('n', 'C-q', '<cmd> q <CR>', opts)
+vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
 
--- delete a single char without saving it into the register
-vim.keymap.set('n', 'x','"_x', opts)
+-- delete single character without copying into register
+vim.keymap.set('n', 'x', '"_x', opts)
 
--- vertical scroll and center
+-- Vertical scroll and center
 vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
 vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
 
--- find and center
+-- Find and center
 vim.keymap.set('n', 'n', 'nzzzv', opts)
 vim.keymap.set('n', 'N', 'Nzzzv', opts)
 
--- Resize tabs with arrows
+-- Resize with arrows
 vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
 vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
 vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
@@ -36,14 +37,14 @@ vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
 -- Buffers
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
-vim.keymap.set('n', '<leader>x', ':Bdelete!<CR>', opts) -- close buffer
+vim.keymap.set('n', '<leader>x', ':bdelete!<CR>', opts) -- close buffer
 vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
 
 -- Window management
 vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- split window vertically
 vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
-vim.keymap.set('n', '<leader>se', '<C-w>=', opts) 
-vim.keymap.set('n', '<leader>xs', ':close<CR>', opts)
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
 
 -- Navigate between splits
 vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
@@ -51,20 +52,20 @@ vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
 vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
 vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
 
--- Tabs 
-vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts)
-vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts)
-vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts)
-vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts)
+-- Tabs
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
 
--- Toggle line wrapping 
+-- Toggle line wrapping
 vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
 
--- Stay in v mode
+-- Stay in indent mode
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
 
--- keep last yanked
+-- Keep last yanked when pasting
 vim.keymap.set('v', 'p', '"_dP', opts)
 
 -- Diagnostic keymaps
@@ -78,14 +79,3 @@ end, { desc = 'Go to next diagnostic message' })
 
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
-
-
-
-
-
-
-
-
-
-
