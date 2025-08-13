ed29b834f1ac3ea8408e2fcfd9d87eb031febace
dilshodturab <avlomberdioglid@gmail.com>
2025-08-13 14:45:16 +0500

    tab indent size were different for different file types so it is fixed

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,48 +1,48 @@
 return {
-  'nvimtools/none-ls.nvim',
+  "nvimtools/none-ls.nvim",
   dependencies = {
-    'nvimtools/none-ls-extras.nvim',
-    'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
+    "nvimtools/none-ls-extras.nvim",
+    "jayp0521/mason-null-ls.nvim", -- ensure dependencies are installed
   },
   config = function()
-    local null_ls = require 'null-ls'
+    local null_ls = require "null-ls"
     local formatting = null_ls.builtins.formatting -- to setup formatters
     local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
     -- Formatters & linters for mason to install
-    require('mason-null-ls').setup {
+    require("mason-null-ls").setup {
       ensure_installed = {
-        'prettier', -- ts/js formatter
-        'eslint_d', -- ts/js linter
-        'biome', -- new js ts formatter
-        'shfmt', -- Shell formatter
-        'checkmake', -- linter for Makefiles
-        -- 'stylua', -- lua formatter; Already installed via Mason
-        -- 'ruff', -- Python linter and formatter; Already installed via Mason
+        "prettier", -- ts/js formatter
+        "eslint_d", -- ts/js linter
+        "biome", -- new js ts formatter
+        "shfmt", -- Shell formatter
+        "checkmake", -- linter for Makefiles
+        "stylua", -- lua formatter; Already installed via Mason
+        "ruff", -- Python linter and formatter; Already installed via Mason
       },
       automatic_installation = true,
     }
 
     local sources = {
       diagnostics.checkmake,
-      formatting.biome.with { filetypes = { 'javascript', 'typescript', 'css' } },
-      formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown', 'vue' } },
+      formatting.biome.with { args = { "-i", "2" }, filetypes = { "javascript", "typescript", "css" } },
+      formatting.prettier.with { args = { "-i", "2" }, filetypes = { "html", "json", "scss", "sass", "yaml", "markdown", "vue" } },
       formatting.stylua,
-      formatting.shfmt.with { args = { '-i', '4' } },
+      formatting.shfmt.with { args = { "-i", "2" } },
       formatting.terraform_fmt,
-      require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
-      require 'none-ls.formatting.ruff_format',
+      require("none-ls.formatting.ruff").with { extra_args = { "--extend-select", "I" } },
+      require "none-ls.formatting.ruff_format",
     }
 
-    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     null_ls.setup {
       -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
       sources = sources,
       -- you can reuse a shared lspconfig on_attach callback here
       on_attach = function(client, bufnr)
-        if client:supports_method 'textDocument/formatting' then
+        if client:supports_method "textDocument/formatting" then
           vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd('BufWritePre', {
+          vim.api.nvim_create_autocmd("BufWritePre", {
             group = augroup,
             buffer = bufnr,
             callback = function()
